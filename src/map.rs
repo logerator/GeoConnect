a618017569293e45d52fc4cfc24e5f4b163dada3
Russell489 <russelletzler@gmail.com>
2026-06-17 14:39:48 +0900

    Map center

--- src/map.rs
+++ src/map.rs
@@ -3,22 +3,24 @@ use dioxus::prelude::*;
 pub fn JapanMap() -> Element {
     rsx! {
         div {
-            class: "p-8",
+            id: "japan-map-section",
 
             h1 {
-                class: "text-4xl font-bold mb-4",
                 "Japan Regions Map"
             }
 
             p {
-                class: "mb-4 text-gray-600",
                 "Click a region to learn more."
             }
 
-            img {
-                src: asset!("/assets/japan.svg"),
-                width: "700px",
-                alt: "Map of Japan regions"
+            div {
+                id: "japan-map-wrapper",
+
+                img {
+                    src: asset!("/assets/japan.svg"),
+                    id: "japan-map-img",
+                    alt: "Map of Japan regions"
+                }
             }
         }
     }
