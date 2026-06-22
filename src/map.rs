3ca9e7943d1fad6da60973036decd5e23e2d1d83
logerator <logancanady3@gmail.com>
2026-06-22 21:29:56 +0900

    updated README

--- src/map.rs
+++ src/map.rs
@@ -11,35 +11,26 @@ use crate::regions::KyushuBtn;
 #[component]
 pub fn JapanMap() -> Element {
     rsx! {
-        div {
-            class: "flex flex-col items-center justify-center h-screen",
+        div { class: "flex flex-col items-center justify-center h-screen",
 
-            h1 {
-                class: "text-4xl font-bold mb-4",
-                "Japan Regions Map"
-            }
+            h1 { class: "text-4xl font-bold mb-4", "Japan Regions Map" }
 
-            p {
-                class: "mb-4 text-gray-600",
-                "Click a region to learn more."
-            }
+            p { class: "mb-4 text-gray-600", "Click a region to learn more." }
 
-            div {
-                class: "map-container",
+            div { class: "map-container",
                 img {
                     src: asset!("/assets/japan.svg"),
                     width: "700px",
                     alt: "Map of Japan regions",
                 }
-                div {
-                    class: "region-buttons",
-                    HokkaidoBtn {},
-                    TohokuBtn {},
-                    ChubuBtn {},
-                    KantoBtn {},
-                    KansaiBtn {},
-                    ChugokuBtn {},
-                    ShikokuBtn {},
+                div { class: "region-buttons",
+                    HokkaidoBtn {}
+                    TohokuBtn {}
+                    ChubuBtn {}
+                    KantoBtn {}
+                    KansaiBtn {}
+                    ChugokuBtn {}
+                    ShikokuBtn {}
                     KyushuBtn {}
                 }
             }
