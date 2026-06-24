ea7f54b8ae0e6a7cfefb20f2a83d9a067a3c35b9
logerator <logancanady3@gmail.com>
2026-06-24 13:28:33 +0900

    restore region buttons to map.rs

--- src/map.rs
+++ src/map.rs
@@ -11,24 +11,27 @@ use crate::regions::KyushuBtn;
 #[component]
 pub fn JapanMap() -> Element {
     rsx! {
-        div {
-            id: "japan-map-section",
+        div { class: "flex flex-col items-center justify-center h-screen",
 
-            h1 {
-                "Japan Regions Map"
-            }
+            h1 { class: "text-4xl font-bold mb-4", "Japan Regions Map" }
 
-            p {
-                "Click a region to learn more."
-            }
+            p { class: "mb-4 text-gray-600", "Click a region to learn more." }
 
-            div {
-                id: "japan-map-wrapper",
-
+            div { class: "map-container",
                 img {
                     src: asset!("/assets/japan.svg"),
-                    id: "japan-map-img",
-                    alt: "Map of Japan regions"
+                    width: "700px",
+                    alt: "Map of Japan regions",
+                }
+                div { class: "region-buttons",
+                    HokkaidoBtn {}
+                    TohokuBtn {}
+                    ChubuBtn {}
+                    KantoBtn {}
+                    KansaiBtn {}
+                    ChugokuBtn {}
+                    ShikokuBtn {}
+                    KyushuBtn {}
                 }
             }
         }
