fa5a99c401b7974f8508414bc26ec431de0baf58
Russell489 <russelletzler@gmail.com>
2026-06-30 11:15:32 +0900

    Made the map text look cleaner

--- src/map.rs
+++ src/map.rs
@@ -1,4 +1,5 @@
 use dioxus::prelude::*;
+
 use crate::regions::HokkaidoBtn;
 use crate::regions::TohokuBtn;
 use crate::regions::ChubuBtn;
@@ -11,18 +12,22 @@ use crate::regions::KyushuBtn;
 #[component]
 pub fn JapanMap() -> Element {
     rsx! {
-        div { class: "flex flex-col items-center justify-center min-h-screen",
+        div { class: "japan-map-content",
 
-            h1 { class: "text-4xl font-bold mb-4", "Japan Regions Map" }
+            h1 { class: "map-title",
+                "Japan Regions Map"
+            }
 
-            p { class: "mb-4 text-gray-600", "Click a region to learn more." }
+            p { class: "map-subtitle",
+                "Click a region to learn more"
+            }
 
             div { class: "map-container",
                 img {
                     src: asset!("/assets/japan.svg"),
-                    width: "700px",
                     alt: "Map of Japan regions",
                 }
+
                 div { class: "region-buttons",
                     HokkaidoBtn {}
                     TohokuBtn {}
