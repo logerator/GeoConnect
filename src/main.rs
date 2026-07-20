d627860e692e0dd432def5b63111e06294ce2434
Russell489 <russelletzler@gmail.com>
2026-07-20 21:02:08 +0900

    Completed interactive buttons

--- src/main.rs
+++ src/main.rs
@@ -47,8 +47,9 @@ pub enum Route {
 
 
 const FAVICON: Asset = asset!("/assets/favicon.ico");
-const MAIN_CSS: Asset = asset!("/assets/main.css");
+
 const STYLES_CSS: Asset = asset!("/assets/styles.css");
+const MAIN_CSS: Asset = asset!("/assets/main.css");
 const HEADER_SVG: Asset = asset!("/assets/header.svg");
 
 #[tokio::main]
